b77dde93b35946a573b046effa934189e2cb2478
Andrii Zadorozhnii <zador746@gmail.com>
2024-03-20 19:44:24 -0700

    box border change

---
+++ index.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.range_box input').forEach(item => {
+
+    item.addEventListener('mousemove', function() {
+
+        this.closest('.range_box').querySelector('.rangeValue').innerHTML = this.value;
+    })
+
+});
+
+const myBox = document.getElementById('box');
+const borderRadiusInput = document.getElementById('borderRadiusInput');
+
+borderRadiusInput.addEventListener('mousemove', () => {
+    const borderRadiusInputValue = borderRadiusInput.value;
+    myBox.style.borderRadius = borderRadiusInputValue + 'px';
+});
